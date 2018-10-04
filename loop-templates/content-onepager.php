a14012ef98dc20a6ed6632620ce6f0d30e4985c9
heyohbunkbunk <mdroberts@vcu.edu>
2018-10-04 11:11:46 -0400

    Lots of css and main homepage stuff
    just tons

--- loop-templates/content-onepager.php
+++ loop-templates/content-onepager.php
@@ -22,32 +22,189 @@
 				<div class="foot-title">FOOTPRINTS ON THE JAMES</div>
 		</div>
 		<div class="sub-title">The Natural and Human History of the James River Watershed</div>
+		<div class="scroll-to-content-arrow">
+		<a href="#homepage-content"><img src="<?php echo get_stylesheet_directory_uri();?>/imgs/arrow-down-white.svg"></a>
+		</div>
 		<div class="vert-path">
 			<div class="fotj-content">
-				<h2 class="fotj-titles">About this Course</h2>
+				<h2 class="lead-title" id="homepage-content">About this Course</h2>
 				<p>
-					<span class="drop-cap">T</span>he principal goal for this class is to expose the students to the importance of the James River watershed to the residents of Virginia in the present, past and future. This collaborative, experiential learning opportunity will combine faculty from the Biology Department, the Center for Environmental Studies and the Outdoor Adventure Program. By immersing students in a natural landscape prominent in Virginia’s history for four weeks, backpacking and canoeing through the watershed and down the river, this program represents a unique opportunity for students at VCU to learn about human history, natural history and how the two have shaped each other.
+					<span class="drop-cap">T</span>he principal goal for this class is to expose the students to the importance of the James River watershed to the residents of Virginia in the present, past and future. This collaborative, experiential learning opportunity will combine faculty from the <a href="https://biology.vcu.edu/">Biology Department</a>, the <a href="https://ces.vcu.edu/">Center for Environmental Studies</a> and the <a href="https://recsports.vcu.edu/programs/outdoor-adventure-program/">Outdoor Adventure Program</a>. By immersing students in a natural landscape prominent in Virginia’s history for four weeks, backpacking and canoeing through the watershed and down the river, this program represents a unique opportunity for students at VCU to learn about human history, natural history and how the two have shaped each other &#x1F463;&#x1F43E;.
 				</p>
-				<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14245723867_87e09d7b80_sized.jpg" width="100%">
+				<div>
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14245723867_87e09d7b80_sized.jpg" width="100%">
+				</div>
+				<div class="photo-caption">
+					<h5>This is a caption for this photo. Isn't it nice?</h5>
+				</div>
 				<p>
-					<span class="drop-cap">G</span>oals for each student will be to learn more about the specific features that make up this river system by identifying and stimulating individual ownership of particular issues of interest to each student before and during the class. In addition, students will learn and practice advanced camping, canoeing/kayaking, and outdoor living skills, including leave no trace camping. Students will also learn to function as team members, and will be instructed in expedition management, group dynamics, and leadership skills.
+					<a href="#homepage-goals">Goals</a> for each student will be to learn more about the specific features that make up this river system by identifying and stimulating individual ownership of particular issues of interest to each student before and during the class.
 				</p>
-				<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/dsc_0283_sized.jpg" width="100%">
+				<p>	
+					In addition, students will learn and practice advanced camping, canoeing/kayaking, and outdoor living skills, including <a href="https://lnt.org/learn/7-principles">Leave No Trace camping</a> &#x1F3D5;. Students will also learn to function as team members, and will be instructed in expedition management, group dynamics, and leadership skills &#x1F44D;.
+				</p>
+				<div>
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/dsc_0283_sized.jpg" width="100%">
+				</div>
+				<div class="photo-caption">
+					<h5>This is a caption for this photo. Isn't it nice?</h5>
+				</div>
+			</div>
+			<div>
+				<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14409007516_e91f8e76d0_sized.jpg" class="wide-image" width="100%" height="auto">
+			</div>
+				<div class="photo-caption">
+						<h5>This is a caption for this photo. Isn't it nice?</h5>
+				</div>
+			<div class="fotj-content">
+				<h2>Field lessons on the James River</h2>
 				<p>
-					<span class="drop-cap">T</span>his expedition will be a collaborative field experience between the units indicated above, taking place over five weeks in the summer of 2019. It will begin with a series of topical seminars and skill development classes at the VCU Rice Rivers Center on the James River in Charles City County, VA. The class will continue to investigate the lower James by paddling to the colonial capital at Jamestown. The class will drive to the headwaters of the James River near the Blue Ridge Mountains and proceed by foot, canoe, bateau and kayak through the watershed and down the river back to Richmond. During this journey the students will be engaged with a wide variety of hands-on interactions in the three disciplines, guided by faculty from each department and guest experts. The Outdoor Adventure Program will be providing trained students from their Student Outdoor Leader Program to act as program guides to teach the other students a wide variety of practical outdoors skills and to introduce traditional expedition management technique. The students will be camping out each night and managing their own daily affairs including camp duties and class work, keeping journals and generally making it real.
+					<span class="drop-cap">T</span>his expedition will be a collaborative field experience between the units indicated above, taking place over five weeks in the summer of 2019. It will begin with a series of topical seminars and skill development classes at the <a href="https://ricerivers.vcu.edu/">VCU Rice Rivers Center</a> on the James River in Charles City County, VA. The class will continue to investigate the lower James by paddling to the colonial capital at Jamestown. The class will drive to the headwaters of the James River near the Blue Ridge Mountains &#x1F304; and proceed by foot, canoe, bateau and kayak through the watershed and down the river back to Richmond.
+				</p> 
+			</div>
+			<div class="photo-set">
+				<div class="photo-3">
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14711563705_a7827a5323_sized.jpg" width="auto">
+				</div>
+				<div class="photo-3">
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14648864061_1533010002_sized.jpg" width="auto">
+				</div>
+				<div class="photo-3">
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14452301543_1337ad98b5_sized.jpg" width="auto">
+				</div>
+			</div>
+			<div class="fotj-content">
+				<p>	
+					During this journey the students will be engaged with a wide variety of hands-on interactions in the three disciplines, guided by faculty from each department and guest experts&#x1F984;. The Outdoor Adventure Program will be providing trained students from their Student Outdoor Leader Program as program guides to teach a wide variety of practical outdoors skills and to introduce traditional expedition management and outdoor leadership techniques. The students will be camping in tents each night and managing their own daily affairs. Responsibility for daily activities such as group leadership, cooking, navigation, setting up camp and managing social media will be assigned as daily duties on rotation &#x1F4E3;.
 				</p>
-		</div>
-			<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/dsc_0081_sized.jpg" class="wide-image" width="100%" height="auto">
-		<div class="fotj-content">
-			<h2 class="fotj-titles">Life lessons on the James River</h2>
-			<p>
-				<span class="drop-cap">I</span>psum dolor sit amet, consectetur adipisicing elit. Inventore necessitatibus ipsa maiores numquam impedit explicabo culpa accusantium commodi dolorum quisquam nisi voluptas veritatis ipsum veniam dolores consectetur possimus eius odio neque similique reiciendis enim aspernatur accusamus nostrum qui alias magni voluptate illo consequatur distinctio dolorem saepe blanditiis quod eligendi quam minima nemo minus! Numquam eligendi doloremque dolor mollitia recusandae commodi est dolorem minima consequuntur veniam laborum obcaecati expedita animi amet dolore nam laboriosam quos praesentium sequi velit. Quia aperiam eum dolorum dicta ad neque quae eos ea provident odit non ab reiciendis distinctio laboriosam totam ratione harum temporibus quam fugit quidem debitis impedit libero deleniti odio accusantium rem praesentium! Nulla provident expedita blanditiis labore magnam fugit voluptates repellat dolorem dolor unde ratione accusantium et nihil animi consectetur atque fugiat cumque soluta molestiae quidem iste nesciunt incidunt ea nemo odio vero libero ut neque non at minus quae qui consequatur sit repudiandae a ipsam quo autem deserunt saepe recusandae debitis aut laudantium numquam quibusdam ipsa. Molestiae nemo repellat magni officiis cum eos architecto incidunt quod quos officia cumque ut nisi placeat eum aperiam id similique. Natus dolorum hic necessitatibus cupiditate voluptas voluptates. Corrupti mollitia perferendis porro ex vel minus. Est asperiores.
-			</p>
-			<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14245500070_e75ad38aca_sized.jpg" width="100%">
-			<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14546260603_144fe7a908_sized.jpg" width="100%">
-			<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14452301543_1337ad98b5_sized.jpg" width="100%">
-		</div>
-			<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14504828146_e63e7a356f_sized.jpg" class="wide-image" width="100%" height="auto">
+			</div>
+			<div>
+				<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14504828146_e63e7a356f_sized.jpg" class="wide-image" width="100%" height="auto">
+			</div>
+			<div class="fotj-content">
+				<h2>Trip Life</h2>
+				<p>
+					<span class="drop-cap">A</span>ll participants will be living, travelling and learning in variety of camping locations in close proximity to the James River and its watershed. From the first night out &#x1F30C;, everyone will be living out of a backpacking tent and eating meals prepared by our classmates. After the first week, most of your nights will be spent in a different place from the evening before. While there will be a few opportunities for a night back in Richmond, we will spend ~28 days and nights in the field.
+				</p>
+				<div>
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14465508798_2643e7b259_sized.jpg" width="100%">
+				</div>
+				<div class="photo-caption">
+					<h5>This is a caption for this photo. Isn't it nice?</h5>
+				</div>
+				<p>
+					Showers, grid electricity and refrigeration will be infrequent if not absent. The class will take place during the early summer in Virginia; students will have ample opportunity to consider mosquitos &#x1F99F;, heat, sunburn, ticks, chafing, and body odor &#x1F9DF;. To accommodate this, the day to day operations of this trip will be largely the responsibility of the student participants. This will require both self discipline and cooperation among our members.
+				</p>	
+			</div>
+			<div class="photo-set">
+				<div class="photo-2">
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14708390731_cbb499d913_sized.jpg" width="auto">
+				</div>
+				<div class="photo-2">
+					<img src="<?php echo get_stylesheet_directory_uri();?>/imgs/14525069234_0d948bbdb8_sized.jpg" width="auto">
+				</div>
+			</div>
+			<div class="fotj-content">
+				<p>
+					The living arrangements will be organized enough to maintain a smooth flow of work and life without being burdensome. To this end, you will all be dividing up the necessary group responsibilities, outlined below. These will include participating in educational opportunities, writing daily journal entries &#x270F;, taking pictures &#x1F4F8; and possibly updating various social media outlets when appropriate. Student Trip Leaders will be responsible for making sure that everything that needs to happen does happen each day. Trip Leaders will make all final decisions, delegate authority, and define duties for the others when necessary.
+				</p>
+
+				<div class="wave-break">
+				</div>
+				<h2 id="homepage-goals">Specific Course Goals</h2>
+				<p>
+					Students should be able to:
+					<ul>
+						<li>identify the major features of a river system and its related watershed</li>
+						<li>discuss the distinguishing features of the James River and its watershed</li>
+						<li>identify, compare and discuss features of a biological system of interest in each of the three regions of the river (or equivalent independent project)</li>
+						<li>recognize and understand many of the ways in which human habitation and development have impacted the James River, and vice versa</li>
+						<li>understand & explain connections between the river and the land which surrounds it</li>
+						<li>learn, use and demonstrate proficiency in expedition skills, management, and logistics as it relates to this trip</li>
+					</ul>
+				</p>
+				<div class="wave-break">
+				</div>
+				<h2>Requirements</h2>
+				<p>
+					<h3>Honor Code:</h3> Giving or receiving unauthorized assistance on tests or any other graded activities, as well as plagiarism, are violations of the University's Honor Code and will result in failure and a referral to the Student Honor Court. Also, students who observe cheating or any violation of the Honor Code have a responsibility to follow the procedures detailed in the Virginia Commonwealth University Student Handbook.
+				</p>
+				<p>
+					Participants will be held to the <a href="https://conduct.students.vcu.edu/student-code-of-conduct/">VCU Student Code of Conduct</a> at all times. This document and other relevant information can be found at the following link. Please pay particular attention to the section regarding conduct off-campus.
+				</p>
+				<p>
+					<h3>Required Readings:</h3> TBD. Students will be given a course pack with required readings, articles, etc.
+				</p>
+				<div class="wave-break">
+				</div>
+				<h2>Assessment</h2>
+				<p>
+					<h3>Grades for all assignments will be on this scale:
+						<h4>
+							90-100%=A, 80-89%=B, 70-79%=C, 60-69%=D, 0-59%=F
+						</h4>
+					</h3>
+				</p>
+				<p>
+					This class will be assessed by a variety of traditional assessment techniques, including grading writing assignments, a written examination, and topical presentations. In addition, we plan to have students to undertake individualized and specialized research, and then teach some content of their own. Each student will be expected to keep journals of their observations and experiences during these exercises. Students will also be expected to keep a detailed and analytical personal journal of the class, to serve as an assessment tool as well as an outlet of student creative expression and self reflection.
+				</p>
+				<p>
+					<h3>Field and personal reflection journal:
+						<h4>
+							20% field, 20% reflection
+						</h4>
+					</h3>
+					<ul>
+						<li>1st entry before departure – discuss any expectations and fears you have about this trip/course, etc.</li>
+						<li>Daily reflection – open ended and/or specific questions</li>
+						<li>Field notes – species seen, behavior, ecology, habitats, drawings, etc.</li>
+						<li>Pencil and Pen, Rite in the Rain notebook or paper recommended</li>
+					</ul>
+				<p>
+					<h4>Field Journal:</h4> There will be two types of journal entries for this course. The first will be a natural history observation journal. We will expect daily entries, based on the methods and suggestions outlined in the Farnsworth et al. 2014 paper from the Journal of Natural History Education and Experience.
+				</p>
+				<p>
+					<h4>Reflection Journal:</h4> Students are required to complete a minimum of 12 personal journal entries during the trip. Entries should be 1-2 full pages (front and back) in length. Students should write an entry every 2 days, and entries should analytically connect students’ experiences at the sites, their interactions with speakers/activists, course readings, and the experience of traveling. Your journal entries should be written every day or at least every couple of days.
+				</p>
+				<p>
+					In other words, we don’t want you to go a week without writing anything and then write daily during the last week of the course. It is important for your reflection and analysis to spread out the journal entries across the four weeks. Other than these guidelines, there are no assigned topics for these journal entries. Journals will be collected and checked periodically on the trip. You will turn in your journal at the end of the trip.
+				</p>
+				<p>
+					<h3>Participation and Professionalism:
+						<h4>
+							30%
+						</h4>
+					</h3>
+				</p>
+				<p>
+					Are you engaged in discussions, have you prepared for lectures and discussions by reading the assigned papers, are you excited to learn new skills, are you willing to help out the rest of the field team by stepping in and lending a hand when needed?
+				</p>
+				<p>
+					Is your general conduct with faculty, student leaders and invited speakers professional? This includes both field and “off-hours” for the duration of the course (See the Professionalism Grading Rubric below).
+				</p>
+				<p>
+					<h3>Practical Exam:
+						<h4>
+							15%
+						</h4>
+					</h3>
+				</p>
+				<p>
+					This exam will be an assessment of your comprehension and retention of the material and techniques present up to the end the week at the Rice Center.
+				</p>
+				<p>
+					<h3>End of Course Project/Presentation:
+						<h4>
+							15%?
+						</h4>
+					</h3>
+				</p>
+				<p>
+					This will consist of a final presentation on the final day of class, 6/18. These presentations will be synthetic evaluations of observations made in the field. The topics should relate to course subject material. Topics will be evaluated and approved before we leave the Rice Rivers Center. These will be done in groups of 2.
+				</p>
+				<div class="wave-break">
+				</div>	
+			</div>
 		</div>
 	</div>
 
